604879c3e0e092e5be1e03ddea3c7ff59381e265
Wesley Benites da Silva <42190880+SilvaWesleyB@users.noreply.github.com>
2022-10-27 14:37:35 -0400

    Commit inicial
    Arquivos finais já testados

---
+++ Page_Teste.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace SRC;
+
+include 'src/funcoes.php';
+
+$act = new Funcoes();
+
+/*#################################
+$ano = readline("Insira o Ano: ");
+
+$resultado = $act->SeculoAno($ano);
+
+print_r($resultado);
+
+/*#################################
+
+$numero = readline("Insira um Número: ");
+
+$resultado = $act->PrimoAnterior($numero);
+
+print_r($resultado);
+
+/*#################################
+$arr = array(
+	array(25,22,18),
+	array(10,15,13),
+	array(24,5,2),
+	array(80,17,15)
+);
+
+print_r($act->SegundoMaior($arr));
+
+/*#################################
+
+$arr = [3, 5, 67, 98, 3];
+
+$result = $act->SequenciaCrescente($arr);
+
+if($result === true){
+	print_r("Verdadeiro");
+}else{
+	print_r("Falso");
+}*/
---
+++ README.md
@@ -0,0 +1,15 @@
+<h1>Projeto Infoideias</h1>
+
+<p>
+Projeto realizado como teste de conhecimento para empresa Infoideias a fim de verificar nivel de conhecimento basico da linguagem PHP.
+</p>
+
+<p>
+Para rodar os testes usando Terminal do VSCode, basta executar o comando "php Page_Teste.php".
+
+#####
+
+Lembre-se de mover o "/*" para ativar a solicitação das funções.
+</p>
+
+<footer>Author: Wesley Benites</footer>
